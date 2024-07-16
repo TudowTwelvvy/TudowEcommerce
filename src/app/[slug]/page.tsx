5c00bdf6343f54d0aaf19a2b2d706bb6934a628d
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-07-16 19:58:37 +0200

    completed the slug and  the frontend

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,45 @@
+import Add from '@/components/Add'
+import CustomizeProducts from '@/components/CustomizeProducts'
+import ProductImages from '@/components/ProductImages'
 import React from 'react'
 
-function page() {
-  return <div>slug</div>
+function SinglePage() {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMG */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      {/*text*/}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nisi a
+          voluptatem, quidem quo, eos voluptate culpa rerum exercitationem nemo
+          doloribus! Quis accusantium eos quam deserunt illum dolores voluptatem
+          asperiores.
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">R2900</h3>
+          <h2 className="font-medium text-2xl">R2900</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam,
+            nulla. Alias nam inventore fugit, quas consectetur nisi cupiditate
+            ipsum? Aut quo vero deserunt atque assumenda repudiandae dolorem
+            possimus voluptatibus id.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default page
+export default SinglePage
