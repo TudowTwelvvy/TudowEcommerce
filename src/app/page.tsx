9f15743129050452ec3cf265a623fd70b5529eae
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-07-16 16:31:17 +0200

    completed the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
 
       <div className="mt-24 px-4 md:px-8 lg:px-16 ">
         <h1 className="text-2xl">New Products</h1>
+        <ProductList />
       </div>
     </div>
   )
