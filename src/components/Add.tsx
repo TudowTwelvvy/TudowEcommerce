eac9e30c2db097904adac1d9716ad9a14d6bf1ea
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-07-21 22:52:23 +0200

    completed the filter functionality

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useWixClient } from '@/hooks/useWixClient'
+import { useState } from 'react'
 
-function Add({
+const Add = ({
   productId,
   variantId,
   stockNumber,
@@ -10,49 +11,55 @@ function Add({
   productId: string
   variantId: string
   stockNumber: number
-}) {
+}) => {
   const [quantity, setQuantity] = useState(1)
 
-  //TEMPORARY
-  const stock = 4
+  // const stock = 4;
 
-  const handleQuantity = (type: 'increase' | 'decrease') => {
-    if (type === 'decrease' && quantity > 1) {
+  const handleQuantity = (type: 'i' | 'd') => {
+    if (type === 'd' && quantity > 1) {
       setQuantity((prev) => prev - 1)
     }
-    if (type === 'increase' && quantity < stock) {
+    if (type === 'i' && quantity < stockNumber) {
       setQuantity((prev) => prev + 1)
     }
   }
 
+  const wixClient = useWixClient()
+
   return (
     <div className="flex flex-col gap-4">
-      <h4 className="font-medium">Chooose a Quantity</h4>
+      <h4 className="font-medium">Choose a Quantity</h4>
       <div className="flex justify-between">
         <div className="flex items-center gap-4">
           <div className="bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32">
             <button
-              className="cursor-pointer text-xl"
-              onClick={() => handleQuantity('decrease')}
+              className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
+              onClick={() => handleQuantity('d')}
+              disabled={quantity === 1}
             >
               -
             </button>
             {quantity}
             <button
-              className="cursor-pointer text-xl"
-              onClick={() => handleQuantity('increase')}
+              className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
+              onClick={() => handleQuantity('i')}
+              disabled={quantity === stockNumber}
             >
               +
             </button>
           </div>
-          <div className="text-xs">
-            Only <span className="text-orange-500">4 items</span> left!
-            <br />
-            {''} ("Don't") miss it
-          </div>
+          {stockNumber < 1 ? (
+            <div className="text-xs">Product is out of stock</div>
+          ) : (
+            <div className="text-xs">
+              Only <span className="text-orange-500">{stockNumber} items</span>{' '}
+              left!
+              <br /> {"Don't"} miss it
+            </div>
+          )}
         </div>
-
-        <button className="w-36 text-sm rounded-3xl ring-1 ring-tudow text-tudow py-2 px-4 hover:bg-tudow hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white">
+        <button className="w-36 text-sm rounded-3xl ring-1 ring-tudow text-tudow py-2 px-4 hover:bg-tudow hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none">
           Add to Cart
         </button>
       </div>
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -18,11 +18,36 @@ const ProductList = async ({
 }) => {
   const wixClient = await wixClientServer()
 
-  const res = await wixClient.products
+  const productQuery = wixClient.products
     .queryProducts()
+    .startsWith('name', searchParams?.name || '')
     .eq('collectionIds', categoryId)
+    .hasSome(
+      'productType',
+      searchParams?.type ? [searchParams.type] : ['physical', 'digital']
+    )
+    .gt('priceData.price', searchParams?.min || 0)
+    .lt('priceData.price', searchParams?.max || 999999)
     .limit(limit || PRODUCT_PER_PAGE)
-    .find()
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
+    )
+  // .find();
+
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(' ')
+
+    if (sortType === 'asc') {
+      productQuery.ascending(sortBy)
+    }
+    if (sortType === 'desc') {
+      productQuery.descending(sortBy)
+    }
+  }
+
+  const res = await productQuery.find()
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
