3228e84b7a9c2b9f54f17cf1632ccaf0246cb387
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-07-22 19:45:41 +0200

    edited the cartmodal component

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -11,6 +11,9 @@ function CartModal() {
   const wixClient = useWixClient()
   const { cart, isLoading, removeItem } = useCartStore()
 
+  const arr = cart.lineItems?.map((i) => Number(i.price?.amount))
+  const subTotal = arr?.reduce((acc, num) => acc + num)
+
   return (
     <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
       {!cart.lineItems ? (
@@ -77,7 +80,7 @@ function CartModal() {
           <div className="">
             <div className="flex items-center justify-between font-semibold">
               <span className="">Subtotal</span>
-              <span className="">R{cart.subtotal.amount}</span>
+              <span className="">R{subTotal}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
               Shipping and taxes calculated at checkout.
